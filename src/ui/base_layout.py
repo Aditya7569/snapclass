4de1fe243f083b6f96046b52af4c1b6909f0f3f3
Aditya Singh Rathore <adityasinghrathore7569@gmail.com>
2026-07-27 21:49:09 +0530

    Voice Pipeline

--- src/ui/base_layout.py
+++ src/ui/base_layout.py
@@ -30,19 +30,20 @@ def style_background_dashboard():
                 ,unsafe_allow_html=True)
 
 def style_base_layout():
+    #h
     st.markdown("""
             <style>
             @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis:YEAR@1979&display=swap');
             @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
             /* Hide Top ToolBar of Streamlit*/
-                #MainMenu,footer,header{
-                    visibility:hidden;
-                }
+                # #MainMenu,footer,header{
+                #     visibility:hidden;
+                # }
 
-                .block-container{
-                    padding-top:1.5rem !important;
-                }
+                # .block-container{
+                #     padding-top:1.5rem !important;
+                # }
 
                 h1{
                     font-family: 'Climate Crisis',sans-serif !important;
